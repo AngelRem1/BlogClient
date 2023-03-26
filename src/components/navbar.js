ba598f817af2fed44496e9e33421552de94b02da
Angel Remigio <angelremigio@Angels-MacBook-Pro.local>
2023-03-25 21:51:21 -0700

    removed one unused import

--- src/components/navbar.js
+++ src/components/navbar.js
@@ -7,7 +7,7 @@ import { auth } from "../firebase";
 import "bootstrap/dist/css/bootstrap.css";
 
 // We import NavLink to utilize the react router.
-import { NavLink, useNavigate } from "react-router-dom";
+import { NavLink } from "react-router-dom";
 
 // Here, we display our Navbar
 export default function Navbar() {
